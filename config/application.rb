4d8ff4527f36cb9d2fde251e376029585a8a8f30
Marcin Bunsch <marcin@futuresimple.com>
2012-03-07 21:34:06 +0100

    Added choices gem

--- config/application.rb
+++ config/application.rb
@@ -61,5 +61,8 @@ module Hackstrap
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.from_file 'settings.yml'
+
   end
 end
