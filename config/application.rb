b4eae49902780a797cb523d8a29514a537ecf456
Bart Kiszala <bart@futuresimple.com>
2012-03-07 23:39:25 +0100

    puzzle wip

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ end
 
 module Hackstrap
   class Application < Rails::Application
+    #config.assets.paths << "#{Rails.root}/app/assets"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
