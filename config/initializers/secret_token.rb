dd5940cdcb0b1bcb77319a94335ed4a20d3f2d89
Marcin Bunsch <marcin@futuresimple.com>
2012-03-07 21:28:00 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackstrap::Application.config.secret_token = '2194919cc8d355146dbc5a1d607e7f94aea1cd45a77c75589f68a49119e3b0fc806ed9b9e48a1c1000d5ddf55f9e7b11c328ca13afeb5a3da1f4d8741630d419'
