75aaebbc2f4713b7c0746e37974a98a0c307a04b
Marcin Bunsch <marcin@futuresimple.com>
2012-03-08 13:55:24 +0100

    Added ranking to home page

--- app/assets/javascripts/rank-engine.js
+++ app/assets/javascripts/rank-engine.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  if (window.location.pathname != '/rankings') return;
+  if (window.location.pathname != '/rankings' &&
+      window.location.pathname != '/') return;
   refreshStandings();
   setInterval( function() {
     refreshStandings();
