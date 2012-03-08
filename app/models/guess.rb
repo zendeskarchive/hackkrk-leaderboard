2c6c6fc5a2feb0254a68875761a7d0e7bb59c21e
Marcin Bunsch <marcin@futuresimple.com>
2012-03-08 19:36:58 +0100

    Adding how many users solved a puzzle

--- app/models/guess.rb
+++ app/models/guess.rb
@@ -2,6 +2,8 @@ class Guess < ActiveRecord::Base
   belongs_to :puzzle
   belongs_to :user
 
+  scope :solved, where(:correct => true)
+
   class << self
     def validate_answer(puzzle, guess, user)
       correct = puzzle.answer.to_s.downcase == guess.to_s.strip.downcase
