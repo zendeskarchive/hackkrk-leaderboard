916f51856ea9fcf6d960f55d0d5f03cf9f272bcb
Marcin Bunsch <marcin@futuresimple.com>
2012-03-08 03:09:58 +0100

    Remove the secret application token from the repo and make it loaded via ENV

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Hackstrap::Application.config.secret_token = '2194919cc8d355146dbc5a1d607e7f94aea1cd45a77c75589f68a49119e3b0fc806ed9b9e48a1c1000d5ddf55f9e7b11c328ca13afeb5a3da1f4d8741630d419'
+Hackstrap::Application.config.secret_token = Rails.configuration.secret_token
